2b2e872806337f9868ae489607acd8dca88862f0
shreyash015 <shreyash015@gmail.com>
2023-04-30 22:16:25 +0530

    supermanCR7

--- src/views/ProfileCreation.vue
+++ src/views/ProfileCreation.vue
@@ -45,7 +45,7 @@ import CircleStencil from '@/components/CircleStencil.vue';
                 clip-rule="evenodd"></path>
             </svg>
           </div>
-          <input id="datepickerEle" datepicker type="text"
+          <input id="datepickerEle" datepicker type="text" readonly
             class="hover:cursor-pointer mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Select date">
         </div>
